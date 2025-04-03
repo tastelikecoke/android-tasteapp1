6bf61c68b13f2cd1c17d0420166e84cf90f49b77
tastelikecoke <jmerc87@gmail.com>
2025-04-03 21:14:18 +0800

    Add remove entry button that works

--- app/src/main/java/com/example/tasteapp1/MainActivity.kt
+++ app/src/main/java/com/example/tasteapp1/MainActivity.kt
@@ -20,6 +20,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,9 +35,7 @@ class MainActivity : ComponentActivity() {
             TasteApp1Theme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     TodoList(
-                        { getData(this, it) },
-                        { a, b -> storeData(this, a, b) },
-                        { getDataCount(this) },
+                        DataAccessor(this)
                     )
                 }
             }
@@ -39,52 +43,109 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-fun storeData(activity: MainActivity, data: String, index: Int) {
-    val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
-    with(sharedPref.edit()) {
-        putString("tasteApp1Note${index}", data)
-        apply()
+interface IDataAccessor {
+    fun storeData(data: String, index: Int): Unit
+    fun getData(index: Int): String
+    fun getDataCount(): Int
+    fun setDataCount(count: Int): Unit
+    fun removeData(data: String, index: Int): Unit
+}
+
+class DataAccessor(val activity : MainActivity) : IDataAccessor {
+    override fun storeData(data: String, index: Int): Unit {
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putString("tasteApp1Note${index}", data)
+            apply()
+        }
     }
-}
-fun getData(activity: MainActivity, index: Int): String {
-    val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return ""
-    return sharedPref.getString("tasteApp1Note${index}", "") ?: return ""
-}
+    override fun getData(index: Int): String {
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return ""
+        return sharedPref.getString("tasteApp1Note${index}", "") ?: return ""
+    }
 
-fun getDataCount(activity: MainActivity): Int {
-    val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return 1
-    return sharedPref.getInt("tasteApp1Count", 1) ?: return 1
-}
+    override fun getDataCount(): Int {
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return 1
+        return sharedPref.getInt("tasteApp1Count", 1) ?: return 1
+    }
 
-@Composable
-fun TodoList(getData: (Int) -> String, storeData: (String, Int) -> Unit, getDataCount: () -> Int, modifier: Modifier = Modifier) {
-    Column {
+    override fun setDataCount(count: Int): Unit {
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            putInt("tasteApp1Count", count)
+            apply()
+        }
+    }
+
+    override fun removeData(data: String, index: Int): Unit {
         val count = getDataCount()
-        for (i in 0..<count)
-            Entry(getData, storeData, i, modifier)
+        val sharedPref = activity.getPreferences(Context.MODE_PRIVATE) ?: return
+        with(sharedPref.edit()) {
+            for (i in index..<count)
+                putString("tasteApp1Note${i}", getData(i+1))
+            apply()
+        }
+
+        setDataCount(count - 1)
     }
 }
 
+
 @Composable
-fun Entry(getData: (Int) -> String, storeData: (String, Int) -> Unit, index: Int, modifier: Modifier = Modifier) {
-    var text by remember { mutableStateOf(getData(index)) }
+fun TodoList(accessor: IDataAccessor, modifier: Modifier = Modifier) {
+    Column {
+        val count = accessor.getDataCount()
+        for (i in 0..<count)
+            Entry(accessor, i, modifier)
 
-    OutlinedTextField(
-        value = text,
-        onValueChange = {
-            text = it
-            storeData(it, index)
-        },
-        label = { Text("Note") },
-        modifier = modifier.padding(48.dp)
+        Button(
+            onClick = {
+                accessor.setDataCount(count + 1)
+            },
+            modifier = modifier.padding(12.dp)
+        ) {
+            Text("Add")
+        }
+    }
 
-    )
+
+}
+
+@Composable
+fun Entry(accessor: IDataAccessor, index: Int, modifier: Modifier = Modifier) {
+    var text by remember { mutableStateOf(accessor.getData(index)) }
+    Row( ) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+                accessor.storeData(it, index)
+            },
+            label = { Text("Note") }
+
+        )
+
+        IconButton(
+            onClick = { accessor.removeData("", index) },
+        ) {
+            Icon(Icons.Default.Clear, contentDescription = "Remove")
+        }
+    }
+}
+
+
+class DummyAccessor : IDataAccessor {
+    override fun storeData(data: String, index: Int): Unit {}
+    override fun getData(index: Int): String { return "Note" }
+    override fun getDataCount(): Int { return 2 }
+    override fun setDataCount(count: Int): Unit { return }
+    override fun removeData(data: String, index: Int): Unit {}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun TodoListPreview() {
     TasteApp1Theme {
-        TodoList({ "preview" }, { _, _ -> Unit }, { 1 })
+        TodoList(DummyAccessor())
     }
 }
